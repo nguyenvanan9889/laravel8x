b710e9ef4740f889c26c999881c4e8c04a8a0fb3
nguyenvanan9889@gmail.com <nguyenvanan9889@gmail.com>
2022-01-19 01:04:05 +0700

    m

--- core/annv/demo/routes/web.php
+++
@@ -1,4 +0,0 @@
-<?php
-Route::group(['namespace' => 'Annv\Demo\Http\Controllers'], function(){
-	Route::get('annv-demo-package', 'DemoController@annvDemoPage');
-});
--- core/annv/demo/src/Models/AnnvDemoPackage.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace Annv\Demo\Models;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-class AnnvDemoPackage extends Model {
-	use HasFactory;
-}
