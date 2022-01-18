857513e235d4fe9379174cdf9ee7626444530d63
nguyenvanan9889@gmail.com <nguyenvanan9889@gmail.com>
2022-01-19 01:03:05 +0700

    m

---
+++ vendor/annv/demo/src/Models/AnnvDemoPackage.php
@@ -0,0 +1,7 @@
+<?php
+namespace Annv\Demo\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+class AnnvDemoPackage extends Model {
+	use HasFactory;
+}
