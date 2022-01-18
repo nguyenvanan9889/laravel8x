6533bd5c510c57927c66d431909e68fe7b78f7a9
nguyenvanan9889@gmail.com <nguyenvanan9889@gmail.com>
2022-01-19 01:54:25 +0700

    m

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -16,7 +16,7 @@ class HomeController extends Controller
     public $payment;
     public function __construct(PayInterface $pay, DbInterface $db, LogInterface $log, PaymentInterface $payment = null)
     {
-        $blo = 'blo';
+        $blo = 'dlo';
         $this->pay = $pay;
         $this->db = $db;
         $this->log = $log;
